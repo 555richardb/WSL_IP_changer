6c36dd8889c8769905dab32b6c8edace43de2466
Richard B <111566536+555richardb@users.noreply.github.com>
2023-01-10 14:51:59 +0000

    init

---
+++ setup_ip.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CURRENT_IP=$( ip a | grep -E "inet.*eth0" | awk '{ print substr( $2, 1, length($2)-3 ) }')
+OLD_IP=$(sudo cat /mnt/c/Windows/System32/drivers/etc/hosts | grep door12 |awk '{ print($1)  }')
+if [ $CURRENT_IP != $OLD_IP ]; then
+   sudo sed -i  "s|${OLD_IP}|${CURRENT_IP}|"  /mnt/c/Windows/System32/drivers/etc/hosts
+   echo "Updated"
+else
+   echo $CURRENT_IP $OLD_IP
+fi
